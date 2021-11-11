163503e10ba2490323b097d65fd762386da55d94
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-11-11 17:18:53 -0300

    migrations concluído

---
+++ src/data/Migrations.ts
@@ -0,0 +1,41 @@
+import { BaseDatabase } from './BaseDatabase'
+
+class Migrations extends BaseDatabase {
+    public async main() {
+        try {
+            await this.getConnection().raw(`
+            CREATE TABLE IF NOT EXISTS lama_bands (
+                id VARCHAR(255) PRIMARY KEY,
+                name VARCHAR(255) UNIQUE NOT NULL,
+                music_genre VARCHAR(255) NOT NULL,
+                responsible VARCHAR(255) UNIQUE NOT NULL 
+              );
+
+              CREATE TABLE IF NOT EXISTS lama_shows (
+                id VARCHAR(255) PRIMARY KEY,
+                week_day VARCHAR(255) NOT NULL,
+                start_time INT NOT NULL,
+                end_time INT NOT NULL,
+                band_id VARCHAR(255) NOT NULL,
+                FOREIGN KEY(band_id) REFERENCES lama_bands(id)
+              );
+
+              CREATE TABLE IF NOT EXISTS lama_users (
+                id VARCHAR(255) PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                role VARCHAR(255) NOT NULL DEFAULT "NORMAL"
+              );
+            `)
+
+            console.log('Tabelas criadas')
+        } catch (error: any) {
+            console.log(error)
+        } finally {
+            BaseDatabase.destroyConnection()
+        }
+    }
+}
+
+new Migrations().main()
--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -1,9 +1,9 @@
-import { BaseDatabase } from "./BaseDatabase";
-import { User } from "../model/User";
+import { BaseDatabase } from "./BaseDatabase"
+import { User } from "../model/User"
 
 export class UserDatabase extends BaseDatabase {
 
-  private static TABLE_NAME = "";
+  private static TABLE_NAME = "lama_users"
 
   public async createUser(
     id: string,
@@ -22,8 +22,8 @@ export class UserDatabase extends BaseDatabase {
           role
         })
         .into(UserDatabase.TABLE_NAME);
-    } catch (error) {
-      throw new Error(error.sqlMessage || error.message);
+    } catch (error: any) {
+      throw new Error(error.sqlMessage || error.message)
     }
   }
 
@@ -31,9 +31,9 @@ export class UserDatabase extends BaseDatabase {
     const result = await this.getConnection()
       .select("*")
       .from(UserDatabase.TABLE_NAME)
-      .where({ email });
+      .where({ email })
 
-    return User.toUserModel(result[0]);
+    return User.toUserModel(result[0])
   }
 
 }
